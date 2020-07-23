09b1ba415ef61fd937cb373fb76e01641e5055ad
serzit <serzit@yandex.ru>
2020-07-23 22:36:51 +0300

    Updates

---
+++ precache-manifest.b372864d50f1df6ac7f8aa0aaf6e044f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e69a359db0ee08cafa2b1aed6e86108c",
+    "url": "/video-watcher/index.html"
+  },
+  {
+    "revision": "257ffcb58510cecca0c8",
+    "url": "/video-watcher/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ae22672d9969cdcafedf",
+    "url": "/video-watcher/static/js/2.6de4d486.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/video-watcher/static/js/2.6de4d486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257ffcb58510cecca0c8",
+    "url": "/video-watcher/static/js/main.59a503a4.chunk.js"
+  },
+  {
+    "revision": "0ee5e7211a990c7449f4",
+    "url": "/video-watcher/static/js/runtime-main.e644c48b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/video-watcher/static/media/logo.5d5d9eef.svg"
+  }
+]);
