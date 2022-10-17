f62655abd01bc68b7c5e9c03d2056a8644f993d8
Sergey Borisov <62622751+serzit@users.noreply.github.com>
2022-10-17 19:58:05 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import useVideos from '../hooks/useVideos';
 
 const App = () => {
 	const [selectedVideo, setSelectedVideo] = useState(null);
-	const [videos, search] = useVideos('music');
+	const [videos, search] = useVideos('ragio programming');
 
 	useEffect(() => {
 		setSelectedVideo(videos[0]);
